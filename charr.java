99d2bf26cd153ed8ae6e3236c19088f8ed962934
Atharv Pujari <atharvpujari0020gamil.com>
2026-01-03 21:14:46 +0530

    day 3
    day 3

---
+++ charr.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class charr{
+    public static void main(String arg[]){
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        for(int i=1 ; i<=n; i++){
+            for(int j=i; j<= i+1; j++){ 
+                System.out.print(j);
+
+            }
+            System.out.println();
+        }
+    }
+}
+
+// practice continued
+
+// practice continued
+
