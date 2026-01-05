006b82da800dd009e0fc3087d3e59f1c9a2f0e2b
Atharv Pujari <atharvpujari0020gamil.com>
2026-01-05 10:19:41 +0530

    day 5 moring

---
+++ Function_Methods/FUNCTIONS_QUESTIONS.java
@@ -0,0 +1,28 @@
+
+import java.util.*;
+
+public class FUNCTIONS_QUESTIONS {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("ENTR A : ");
+
+        int a = sc.nextInt();
+
+        System.out.println("ENTR B : ");
+
+        int b = sc.nextInt();
+        System.out.println("ENTR B : ");
+        int c = sc.nextInt();
+
+
+        System.out.println(queone(a, b, c));
+    }
+
+    public static int queone(int a, int b, int c) {
+
+        int avg = Math.av
+
+        return avg;
+
+    }
+}
