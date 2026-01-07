a50e741c3e1fb1bc11ba9626b9d2259a977da46d
Atharv <134258029+Athxrvv18@users.noreply.github.com>
2026-01-07 23:42:44 +0530

    day 7 light

---
+++ Pattern II/practice.java
@@ -0,0 +1,12 @@
+public class practice {
+    public static void main(String args[]){
+        int n = 8;
+        for(int lines = 1; lines<=n; lines++){
+            for(int num=1; num<=lines; num++){
+                System.out.print(num);
+            }
+            System.err.println();
+        }
+    }
+    
+}
---
+++ Pattern II/ps.java
@@ -0,0 +1,11 @@
+public class ps {
+    public static void main(String[] args) {
+        int n = 4;
+        for(int l=1; l<=n; l++){
+            for(int j=1; j<=l; j++){
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+}
