392b66d6ac2abfc95d846c4788bd696a34646f56
Atharv <134258029+Athxrvv18@users.noreply.github.com>
2026-02-02 06:36:49 +0530

    day 17
    dsa 1 que trapping rain water

---
+++ Array II/trappingWater.java
@@ -0,0 +1,36 @@
+public class trappingWater {
+
+    public static void main(String[] args) {
+
+        int arr[] = { 4,2,0,6,3,2,5 };
+        System.out.println(TrapWater(arr));
+    }
+
+    public static int TrapWater(int arr[]) {
+
+        int n = arr.length;
+
+        int leftMax[] = new int[n];
+        leftMax[0] = arr[0];
+
+        for (int i = 1; i < n; i++) {
+            leftMax[i] = Math.max( arr[i] , leftMax[i-1]);
+        }
+
+        int rightMax[] = new int[n];
+        rightMax[n - 1] = arr[n - 1];
+
+        for (int i = n - 2; i >= 0; i--) {
+            rightMax[i] = Math.max( arr[i] , rightMax[i+1]);
+        }
+
+        int trapwater=0;
+
+        for (int i = 1; i < n; i++) {
+            int waterLevel = Math.min(leftMax[i], rightMax[i]);
+            trapwater += waterLevel - arr[i];
+        }
+
+        return trapwater;
+    }
+}
